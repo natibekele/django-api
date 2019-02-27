e37c6d395cdac0b8424e75bb0fbed7e4b2c7ee9a
Nathan Shanko <natibekele@gmail.com>
2019-02-26 20:13:38 -0500

    added create ingredient enpoint

--- app/recipe/tests/test_ingredient.py
+++ app/recipe/tests/test_ingredient.py
@@ -64,7 +64,26 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(len(res.data),1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
 
-    def test_create_ingredient(self):
-        """Test creating an ingredient"""
+    def test_create_ingredient_successful(self):
+        """Test creating an ingredient successfully"""
+        payload = {'name': 'Pepper'}
+
+        self.client.post(INGERDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+
+        res = self.client.post(INGERDIENTS_URL,payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
 
 
